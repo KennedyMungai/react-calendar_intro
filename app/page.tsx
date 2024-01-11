d344c58554ed02caf17830f1e2faea979ddfcdb3
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-01-11 14:51:38 +0300

    Rendered out the Calendar component

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,9 @@
+import CalendarComponent from "@/components/Calendar";
+
 export default function Home() {
   return (
-    <main className="min-h-screen bg-stone-300"></main>
+    <main className="min-h-screen bg-stone-300">
+      <CalendarComponent />
+    </main>
   )
 }
