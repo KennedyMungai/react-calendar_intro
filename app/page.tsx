bb7593c389ea76a57f1a7e9209b40afb97a51ad3
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-01-11 14:33:31 +0300

    Added a background color to the page

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,6 @@ import Image from 'next/image'
 
 export default function Home() {
   return (
-    <main className="min-h-screen"></main>
+    <main className="min-h-screen bg-stone-300"></main>
   )
 }
